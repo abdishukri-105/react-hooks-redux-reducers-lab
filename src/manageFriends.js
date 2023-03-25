38da03c855005df740597030dd4bf69d75ea47d0
abdishukri <abdishukrimohamed105@gmail.com>
2023-03-25 12:55:26 +0300

    finished lab

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = { friends: [] }, action){
+    switch(action.type){
+      case 'friends/add':
+        return { ...state, friends: [...state.friends, action.payload] }
+      case 'friends/remove':
+        const filteredFriends = state.friends.filter(friend => friend.id !== action.payload);
+        return { ...state, friends: filteredFriends };
+      default:
+        return state; }
+  
+  
+  
+  
+
 }
